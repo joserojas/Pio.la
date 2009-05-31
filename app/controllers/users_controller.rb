954ec49df7f586a52474b951538c06c59d41722e
jrallison <jrallison@gmail.com>
2009-05-31 15:33:27 -0400

    Adding twitter login to the authlogic example

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,13 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(params[:user])
-    if @user.save
-      flash[:notice] = "Account registered!"
-      redirect_back_or_default account_url
-    else
-      render :action => :new
+    @user.save do |result|
+      if result
+        flash[:notice] = "Account registered!"
+        redirect_back_or_default account_url
+      else
+        render :action => :new
+      end
     end
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.oauth_login '/oauth_login', { :controller => 'user_sessions', :action => 'create', :method => 'get' }
+  map.oauth_register '/oauth_register', { :controller => 'users', :action => 'create', :method => 'get' }
+  map.authorize_oauth '/oauth', { :controller => 'oauth', :action => 'authorize', :method => 'get' }
+  
   map.resource :account, :controller => "users"
   map.resources :users
   map.resource :user_session
