15bf7cb49ef9c461e0731813ecffc25e5ed29670
John Allison <jallison@gilt.com>
2009-06-27 02:23:13 -0400

    Removing unneeded routes and updating authlogic_oauth submodule

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.oauth_login '/oauth_login', { :controller => 'user_sessions', :action => 'create', :method => 'get' }
-  map.oauth_register '/oauth_register', { :controller => 'users', :action => 'create', :method => 'get' }
-  
   map.resource :account, :controller => "users"
   map.resources :users
   map.resource :user_session
