f563e7c89f596cb8713a12face9d99dd6bba7e64
jrallison <jrallison@gmail.com>
2009-06-27 02:59:17 -0400

    Bumping again ... *sigh*

--- config/environment.rb
+++ config/environment.rb
@@ -26,7 +26,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "authlogic"
-  config.gem "authlogic-oauth", :lib => "authlogic_oauth", :version => '1.0.3'
+  config.gem "authlogic-oauth", :lib => "authlogic_oauth", :version => '1.0.4'
   config.gem "oauth"
   config.gem "json"
 
