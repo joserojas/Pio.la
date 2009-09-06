cfd114b7fce8cbfba8ca8dc1d35891161c31680b
Ivan Acosta-Rubio <ivan@bakedweb.net>
2009-09-05 20:32:41 -0400

    importing authlogic with twitter oauth

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,6 +1,6 @@
 class UserSession < Authlogic::Session::Base
   def self.oauth_consumer
-    OAuth::Consumer.new("z22sSQlQMptg2u0A2wK4w", "hMbGnTbY5nDBMTQq0OLiveDBnqD4BUYQEfTzk5N0wg",
+    OAuth::Consumer.new("7P41WeFU4Mb5XRAIgX3A5w", "OuK2KldHmeKUpLzoHuOgAXvo7u32wyUG4QPC8p7tUEk",
     { :site=>"http://twitter.com",
       :authorize_url => "http://twitter.com/oauth/authenticate" })
   end
